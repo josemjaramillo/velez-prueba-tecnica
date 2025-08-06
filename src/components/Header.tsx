6ebeb226300ef6e4701fc25a94bcd5c9682e4782
Jose M Jaramillo <jmjaramillo.ing@gmail.com>
2025-08-06 11:12:38 -0500

    Add cart logo and animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+import { ShoppingCart } from "lucide-react";
+
 function Header() {
+    const [burst, setBurst] = useState(false);
+    const [carCount, setCarCount] = useState(0);
+
+    const handleClick = () => {
+        setBurst(true);
+        setTimeout(() => setBurst(false), 400); // duración = duración de la animación
+    };
+
     return (
         <nav className="navbar">
             <div className="nav-content container">
                 <h1 className="logo">Vélez</h1>
-                <button className="cart-btn">Carrito</button>
+                <button className="cart-btn" onClick={handleClick}>
+                    <ShoppingCart className="cart-icon" />
+                    {carCount ? <span className="cart-count">{carCount}</span> : null}
+                    {burst && <span className="burst" />}
+                </button>
 
             </div>
         </nav>
